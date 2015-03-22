fddb84ada7af4ea5248627cb5fbd85ce4c402e75
Mitan <dmitrykharkovsky@gmail.com>
2015-03-22 16:37:23 +0800

    improve YahooAPI, add normalization, currencies processing

--- YahooAPI/DataCollector.py
+++ YahooAPI/DataCollector.py
@@ -1,43 +1,145 @@
+import os
 import sys
 import pandas.io.data as web
 import datetime as dt
 
+def normalization_function(target_list):
+    l = len(target_list)
+    answer = []
+    for i in range(1, l):
+        # measure in percent
+        temp = 100.0 * (target_list[i] - target_list[i-1]) / target_list[i]
+        answer.append(temp)
+    return answer
 
-def GetData(startDate, endDate, companyName, mode):
-    start = dt.datetime.strptime(startDate, '%Y-%m-%d')
-    end = dt.datetime.strptime(endDate, '%Y-%m-%d')
-
+def GetCompanyData(startDate, endDate, companyName):
     try:
-        f = web.DataReader(companyName, 'yahoo', start, end)
+        f = web.DataReader(companyName, 'yahoo', startDate, endDate)
         values = f.values
         dates = f.index
     except:
         print "Cant find ", companyName
         return
 
-    if mode == 'C':
-        modeIndex = 3
-    elif mode == 'V':
-        modeIndex = 4
-    else:
-        raise Exception("Imcorrect mode")
-    dates = map(lambda x : x.to_datetime().strftime('%Y-%m-%d'), dates)
-    answer = map(lambda x: round(x[modeIndex], 2), values)
-    return zip(dates, answer)
+    openData = [x[0] for x in values]
+    closeData = [x[3] for x in values]
+    volumeData = [x[4] for x in values]
+
+    answer = [openData, closeData, volumeData]
+    normalized_answer = map(lambda x: normalization_function(x), answer)
+    dates = map(lambda x: x.to_datetime().strftime('%Y-%m-%d'), dates)
+
+    return [dates] + answer + normalized_answer
+
+
+def GetCurrencyData(startDate, endDate, currencyType):
+    try:
+        f = web.DataReader(currencyType, 'fred', startDate, endDate)
+    except:
+        print "Cant find ", currencyType
+        return
+    curData = map(lambda x: x[0], f.values)
+    return [f.index, curData]
+
+
+def process_currencies(folder, start, end):
+    currencies_directory = os.path.join(folder, "currencies")
+    if not os.path.exists(currencies_directory):
+        os.makedirs(currencies_directory)
+
+    normalized_currencies_directory = os.path.join(currencies_directory, "normalized_currencies")
+    if not os.path.exists(normalized_currencies_directory):
+        os.makedirs(normalized_currencies_directory)
+
+    currencies_data = [0 for i in range(3)]
+    currencies = ["USDJPY", "EURUSD", "EURGBP"]
+
+    # usd - jpy
+    dates, currencies_data[0] = GetCurrencyData(start, end, 'DEXJPUS')
+    #eur - usd
+    currencies_data[1] = GetCurrencyData(start, end, 'DEXUSEU')[1]
+    # gbp - usd
+    usd_uk_data = GetCurrencyData(start, end, 'DEXUSUK')[1]
+    #gbp - eur
+    currencies_data[2] = map(lambda x, y: y / x, currencies_data[1], usd_uk_data)
+
+    time_period= len(dates)
+    dates = map(lambda x: x.to_datetime().strftime('%Y-%m-%d'), dates)
+    normalized_currencies = map(lambda x: normalization_function(x), currencies_data)
+
+    for i in range(3):
+        filename = os.path.join(currencies_directory, currencies[i] + ".txt")
+        output_file = open(filename, 'w')
+
+        normalized_filename = os.path.join(normalized_currencies_directory,currencies[i] + "_norm.txt")
+        normalized_output_file = open(normalized_filename, 'w')
+
+        for j in range(time_period):
+            output_file.write("{0}\t{1}\n".format(dates[j], round(currencies_data[i][j], 4)))
+
+        for j in range(1, time_period):
+            normalized_output_file.write("{0}\t{1}\n".format(dates[j], round(normalized_currencies[i][j - 1], 4)))
+
+        output_file.close()
+        normalized_output_file.close()
+
+
+def process_companies(folder, start, end):
+    companies_directory = os.path.join(folder, "companies")
+    if not os.path.exists(companies_directory):
+        os.makedirs(companies_directory)
+
+    normalized_companies_directory = os.path.join(companies_directory, "normalized_companies")
+    if not os.path.exists(normalized_companies_directory):
+        os.makedirs(normalized_companies_directory)
+
+    modes = ["open", "close", "volume"]
+    for company in companies:
+        # data = [dates, openData, closeData, volume, normalizedOpen, normalizedClose, normalizedVolume]
+        data = GetCompanyData(start, end, company)
+        dates = data[0]
+        time_period = len(dates)
+
+        for i in range(3):
+            filename = os.path.join(companies_directory, company + "_" + modes[i] + ".txt")
+            output_file = open(filename, 'w')
+
+            normalized_filename = os.path.join(normalized_companies_directory, company + "_norm_" + modes[i] + ".txt")
+            normalized_output_file = open(normalized_filename, 'w')
+
+            for j in range(time_period):
+                output_file.write("{0}\t{1}\n".format(dates[j], round(data[i + 1][j], 4)))
+
+            for j in range(1, time_period):
+                # data[i+4] is corresponding normalized value, inde j - 1 because of the shift
+                normalized_output_file.write("{0}\t{1}\n".format(dates[j], round((data[i + 4])[j - 1], 4)))
+
+            output_file.close()
+            normalized_output_file.close()
 
 
 if __name__ == "__main__":
+
+    # IXIC is NASDAQ
+    #GSPC is S&P
+    companies = ["AMZN", "AAPL", "BABA", "FB", "GOOG", "QQQ", "YHOO", '^IXIC', '^GSPC']
     """
-    #format
-    startV = '2012-08-01'
-    endV = '2012-08-05'
-    companyName = "GOOGL"
-    mode = 'C'
-    filename = 'C:\\Users\A0134673\\Downloads\\testfile.txt'
-    data = GetData(startV, endV, companyName, mode)
+    start_date = '2015-03-02'
+    end_date = '2015-03-06'
+    folder_name = 'C:/Users/A0134673/Downloads/TestData/'
     """
-    data = GetData(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
-    file = open(sys.argv[5], 'w')
-    for item in data:
-        file.write("{0}\t{1}\n".format(item[0], item[1]))
-    file.close()
+    folder_name = sys.argv[1]
+    start_date = sys.argv[2]
+    end_date = sys.argv[3]
+
+    # transform dates into datetime
+    start_date = dt.datetime.strptime(start_date, '%Y-%m-%d')
+    end_date = dt.datetime.strptime(end_date, '%Y-%m-%d')
+
+    directory = os.path.dirname(folder_name)
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+    process_companies(folder_name, start_date, end_date)
+
+    process_currencies(folder_name, start_date, end_date)
--- YahooAPI/ReadMe.txt.txt
+++ YahooAPI/ReadMe.txt.txt
@@ -1,12 +1,5 @@
 requirement - pandas library
 format for input 
-python DataCollector.py start_date end_date symbol mode output_file_path
+python DataCollector.py folder_path start_date
 
 date is in format YYYY-MM-DD
-mode is either 'V' (volume) or 'C' (close date)
-
-
-format for input 
-python Normalizer.py input_file output_file mode
-
-mode is either 'V' (volume) or 'C' (close date)
